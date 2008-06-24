dcb466374e4ade7e478a2a88064bed3b205d70a6
Larry Diehl <larry@izea.com>
2008-06-23 21:52:14 -0400

    Made MultiProcessSpecRunner extend Spec::Runner::ExampleGroupRunner, and defined load_files

--- lib/paralyze/runner/multi_process_example_group_runner.rb
+++ lib/paralyze/runner/multi_process_example_group_runner.rb
@@ -1,22 +1,19 @@
 module Paralyze
   module Runner
-    # Influenced by MultiProcessSpecRunner in SeleniumGrid
-    class MultiProcessExampleGroupRunner
+    class MultiProcessExampleGroupRunner < Spec::Runner::ExampleGroupRunner
       DEFAULT_MAXIMUM_PROCESSES = 1
-      attr_accessor :maximum_processes, :options, :forker, :example_group_runner_file_path_partitions
+      attr_reader   :partitioned_example_groups      
+      attr_accessor :maximum_processes, :forker
       
-      # TODO: Take in optional err + out StringIO paramaters
       def initialize(options, maximum_processes = DEFAULT_MAXIMUM_PROCESSES)
-        self.options = options
+        super(options)
         self.maximum_processes = maximum_processes || DEFAULT_MAXIMUM_PROCESSES
         self.forker = Forker::ProcessForker
       end
       
       def load_files(file_paths)
-        self.example_group_runner_file_path_partitions = []        
-        file_paths.partition_balanced( concurrent_processes(file_paths.size) ).each do |file_path_partition|
-          self.example_group_runner_file_path_partitions << file_path_partition
-        end
+        super(file_paths)
+        @partitioned_example_groups = @options.example_groups.partition_balanced( concurrent_processes(@options.example_groups.size) )
       end
     
       def run
@@ -24,20 +21,20 @@ module Paralyze
         # test with mock forker
         # fork child_runners and run them
         # reduce success variable
-        options.run_examples
+        @options.run_examples
       end
       
-      # TODO: Refactor this hella ugly method. 
-      # Probably go with a custom OptionsParser that you pass in self.options.argv
-      def child_options(pid, file_paths)
-        child_options = self.options.clone
-        child_options.instance_variable_set(:@files, file_paths)
-        child_options.user_input_for_runner = nil
-        child_format_options = child_options.instance_variable_get(:@format_options)
-        child_format_options[1] = child_output_path(pid)
-        child_options.instance_variable_set(:@format_options, child_format_options)
-        child_options
-      end
+      # # TODO: Refactor this hella ugly method. 
+      # # Probably go with a custom OptionsParser that you pass in self.options.argv
+      # def child_options(pid, file_paths)
+      #   child_options = self.options.clone
+      #   child_options.instance_variable_set(:@files, file_paths)
+      #   child_options.user_input_for_runner = nil
+      #   child_format_options = child_options.instance_variable_get(:@format_options)
+      #   child_format_options[1] = child_output_path(pid)
+      #   child_options.instance_variable_set(:@format_options, child_format_options)
+      #   child_options
+      # end
       
       def child_output_path(pid, output_path = nil)        
         output_path ? "#{output_path}.#{pid}.paralyze" : "#{pid}.paralyze"
--- spec/spec/runner/multi_process_example_group_runner/load_files_spec.rb
+++ spec/spec/runner/multi_process_example_group_runner/load_files_spec.rb
@@ -1,25 +1,33 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Paralyze::Runner::MultiProcessExampleGroupRunner, "#load_files" do  
+  before(:all) do
+    @original_rspec_options = $rspec_options
+  end
+  
   before(:each) do
     options_parser = Spec::Runner::OptionParser.new(StringIO.new, StringIO.new)
-    options_parser.parse [SpecHelper.fixtures_path,"-p","**/*.rb"]
-    @options = options_parser.options
-    @multi_process_example_group_runner = Paralyze::Runner::MultiProcessExampleGroupRunner.new([])
+    options_parser.parse [SpecHelper.fixtures_path,"-p","**/*.rb", "-U", "Paralyze::Runner::MultiProcessExampleGroupRunner"]
+    $rspec_options = @options = options_parser.options
+    @multi_process_example_group_runner = @options.send(:custom_runner)
   end
   
-  it "should set @example_group_runner_file_path_partitions" do
+  after(:each) do
+    $rspec_options = @original_rspec_options    
+  end
+  
+  it "should set partitioned_example_groups" do
     @multi_process_example_group_runner.load_files(@options.files_to_load)
-    @multi_process_example_group_runner.should be_instance_variable_defined(:@example_group_runner_file_path_partitions)
+    @multi_process_example_group_runner.partitioned_example_groups.should_not be_nil
   end
   
-  it "should add subarrays to example_group_runner_file_path_partitions" do
+  it "should add subarrays to partitioned_example_groups" do
     @multi_process_example_group_runner.load_files(@options.files_to_load)
-    @multi_process_example_group_runner.example_group_runner_file_path_partitions.each{|runner| runner.should be_kind_of(Array) }
+    @multi_process_example_group_runner.partitioned_example_groups.each{|group| group.should be_kind_of(Array) }
   end
   
-  it "should use concurrent_processes" do
-    @multi_process_example_group_runner.should_receive(:concurrent_processes).and_return(3)
+  it "should use concurrent_processes based on the number of example groups" do
+    @multi_process_example_group_runner.should_receive(:concurrent_processes).with(6).and_return(3)
     @multi_process_example_group_runner.load_files(@options.files_to_load)    
   end
 end
